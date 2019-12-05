828219568e165f53512e55c33ca24d4ffa1b0852
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-12-05 16:40:55 -0500

    Deletes unused line of code.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,79 @@ class ApplicationController < Sinatra::Base
  end
 
    get '/' do
-     erb "Welcome to Wishable"
+     if Helper.is_logged_in?(session)
+    redirect "/list"
+    else
+      erb :'/homepage'
+    end
    end
+
+ get '/signup' do
+  if Helper.is_logged_in?(session)
+    redirect "/list"
+  else
+      erb :'signup'
+    end
+  end
+
+   post '/signup' do
+      if params["username"] != "" && params["email"] != "" &&  params["password"] != ""
+        @user = User.new(username: params["username"], email: params["email"], password: params["password"])
+        @user.save
+        session[:user_id] = @user.id
+      else
+        redirect "/signup"
+      end
+      redirect :"/list"
+    end
+
+    get '/login' do
+      if !Helper.is_logged_in?(session)
+        erb :'/login'
+      else
+       redirect '/list'
+     end
+    end
+
+    post '/login' do
+     @user = User.find_by(username: params["username"])
+
+     if @user && @user.authenticate(params[:password])
+       session[:user_id] = @user.id
+       redirect '/list'
+     else
+       erb :'/login'
+     end
+    end
+
+    get '/logout' do
+      session.clear
+      redirect to '/login'
+    end
+
+    get '/list' do
+      if !Helper.is_logged_in?(session)
+        redirect '/login'
+      else
+        @user = Helper.current_user(session)
+        erb :'/list'
+    end
+  end
+
+  get '/new_wish_list' do
+   erb :'new_wish_list'
+  end
+
+  post '/new_wish_list' do
+    @user = Helper.current_user(session)
+    @newlist = WishList.create(name: params[:name], user: @user)
+    @newlist.save
+    redirect '/show_wish_list'
+  end
+
+  get '/show_wish_list' do
+    @user = Helper.current_user(session)
+    @newlist = find(params[:id])
+    erb :'show_wish_list'
+  end
 end
