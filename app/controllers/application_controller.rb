ac040dfde3747f342825c87368fa0c03dfcfb589
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-12-06 15:12:05 -0500

    Moves user, wishlist and wishes routes to user, wishlist and wishes controllers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,97 +7,13 @@ class ApplicationController < Sinatra::Base
    set :views, 'app/views'
    enable :sessions
    set :session_secret, "secret"
- end
+  end
 
-   get '/' do
+  get '/' do
      if Helper.is_logged_in?(session)
     redirect "/list"
     else
       erb :'/homepage'
     end
-   end
-
- get '/signup' do
-  if Helper.is_logged_in?(session)
-    redirect "/list"
-  else
-      erb :'signup'
-    end
   end
-
-   post '/signup' do
-      if params["username"] != "" && params["email"] != "" &&  params["password"] != ""
-        @user = User.new(username: params["username"], email: params["email"], password: params["password"])
-        @user.save
-        session[:user_id] = @user.id
-      else
-        redirect "/signup"
-      end
-      redirect :"/list"
-    end
-
-    get '/login' do
-      if !Helper.is_logged_in?(session)
-        erb :'/login'
-      else
-       redirect '/list'
-     end
-    end
-
-    post '/login' do
-     @user = User.find_by(username: params["username"])
-
-     if @user && @user.authenticate(params[:password])
-       session[:user_id] = @user.id
-       redirect '/list'
-     else
-       erb :'/login'
-     end
-    end
-
-    get '/logout' do
-      session.clear
-      redirect to '/login'
-    end
-
-    get '/list' do
-      if !Helper.is_logged_in?(session)
-        redirect '/login'
-      else
-        @user = Helper.current_user(session)
-        erb :'/list'
-    end
-  end
-
-  get '/new_wish_list' do
-   erb :'new_wish_list'
-  end
-
-  post '/new_wish_list' do
-    @user = Helper.current_user(session)
-    @newlist = WishList.create(name: params[:name], user: @user)
-    @newlist.save
-    redirect "/show_wish_list/#{@newlist.id}"
-  end
-
-  get '/show_wish_list/:id' do
-    if !Helper.is_logged_in?(session)
-      redirect '/login'
-    else
-      @user = Helper.current_user(session)
-      @newlist = WishList.find(params[:id])
-      erb :'show_wish_list'
-    end
-  end
-
-  get '/add_wish_to_wishlist/:id' do
-    @wishlist = WishList.find(params[:id])
-    erb :'add_wish_to_wishlist'
-  end
-
-  post '/add_wish_to_wishlist/:wish_list_id' do
-    @wish = Wish.create(name: params[:name], description: params[:description], url: params[:url], wish_list_id: params[:wish_list_id])
-    redirect "/show_wish_list/#{@wish.wish_list_id}"
-  end
-
 end
