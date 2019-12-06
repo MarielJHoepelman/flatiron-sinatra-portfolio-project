0147f97777a5e717a4c03f42509322363c637064
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-12-06 15:16:18 -0500

    Adds `add_wish_to_wishlist` get and post routes to wish controller.

--- app/controllers/wish_controller.rb
+++ app/controllers/wish_controller.rb
@@ -1,4 +1,13 @@
 class WishController <ApplicationController
   # use Rack::Flash
 
+  get '/add_wish_to_wishlist/:id' do
+    @wishlist = WishList.find(params[:id])
+    erb :'add_wish_to_wishlist'
+  end
+
+  post '/add_wish_to_wishlist/:wish_list_id' do
+    @wish = Wish.create(name: params[:name], description: params[:description], url: params[:url], wish_list_id: params[:wish_list_id])
+    redirect "/show_wish_list/#{@wish.wish_list_id}"
+  end
 end
