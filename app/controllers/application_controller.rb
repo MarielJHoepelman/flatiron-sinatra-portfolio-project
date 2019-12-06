9fb46ed6702c969641ca109ba83a2086e3c0c4a5
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-12-06 13:02:19 -0500

    Configures get and post `/add_wish_to_wishlist/:id` routes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -89,4 +89,15 @@ class ApplicationController < Sinatra::Base
       erb :'show_wish_list'
     end
   end
+
+  get '/add_wish_to_wishlist/:id' do
+    @wishlist_id = params[:id]
+    erb :'add_wish_to_wishlist'
+  end
+
+  post '/add_wish_to_wishlist/:wish_list_id' do
+    @wish = Wish.create(name: params[:name], description: params[:description], url: params[:url], wish_list_id: params[:wish_list_id])
+    redirect "/show_wish_list/#{@wish.wish_list_id}"
+  end
+
 end
