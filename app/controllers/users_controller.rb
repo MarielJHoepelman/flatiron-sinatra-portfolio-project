cf5a6dcb03d98838e180997f92cb2e36e60db349
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-12-06 17:53:36 -0500

    Namespaced users controller to `/users`.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,56 +1,52 @@
 class UsersController < ApplicationController
 # use Rack::Flash
-
-  get '/users/:slug' do
-    @user = User.find_by_slug(params[:slug])
-    erb :'/users/index'
-  end
-
-  get '/signup' do
-   if Helper.is_logged_in?(session)
-     redirect "/list"
-   else
-     erb :'signup'
+  namespace '/users' do
+    get '/signup' do
+     if Helper.is_logged_in?(session)
+       redirect "/list"
+     else
+       erb :'signup'
+      end
     end
-  end
 
-  post '/signup' do
-    if params["username"] != "" && params["email"] != "" &&  params["password"] != ""
-      @user = User.new(username: params["username"], email: params["email"], password: params["password"])
-      @user.save
-      session[:user_id] = @user.id
-    else
-      redirect "/signup"
+    post '/signup' do
+      if params["username"] != "" && params["email"] != "" &&  params["password"] != ""
+        @user = User.new(username: params["username"], email: params["email"], password: params["password"])
+        @user.save
+        session[:user_id] = @user.id
+      else
+        redirect "/signup"
+      end
+      redirect :"/list"
     end
-    redirect :"/list"
-  end
 
-  get '/login' do
-    if !Helper.is_logged_in?(session)
-      erb :'/login'
-    else
-      redirect '/list'
+    get '/login' do
+      if !Helper.is_logged_in?(session)
+        erb :'/login'
+      else
+        redirect '/list'
+      end
     end
-  end
 
-  post '/login' do
-    @user = User.find_by(username: params["username"])
+    post '/login' do
+      @user = User.find_by(username: params["username"])
 
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect '/list'
-    else
-      erb :'/login'
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect '/list'
+      else
+        erb :'/login'
+      end
     end
-  end
 
-  get '/logout' do
-    session.clear
-    redirect to '/login'
-  end
+    get '/logout' do
+      session.clear
+      redirect to '/'
+    end
 
-  get '/logout' do
-    session.clear
-    redirect to '/login'
+    get '/:slug' do
+      @user = User.find_by_slug(params[:slug])
+      erb :'/users/index'
+    end
   end
 end
