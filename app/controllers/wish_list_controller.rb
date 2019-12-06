10d7aa2f0ceaea1e7c7da712a797f5bc54ac8b6a
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-12-06 17:54:23 -0500

    Namespaced wish list controller to `/wish_list`.

--- app/controllers/wish_list_controller.rb
+++ app/controllers/wish_list_controller.rb
@@ -10,23 +10,25 @@ class WishListController <ApplicationController
     end
   end
 
-  get '/new_wish_list' do
-    erb :'new_wish_list'
-  end
+  namespace '/wish_list' do
+    get '/new' do
+      erb :'new_wish_list'
+    end
 
-  post '/new_wish_list' do
-    @user = Helper.current_user(session)
-    @newlist = WishList.create(name: params[:name], user: @user)
-    redirect "/show_wish_list/#{@newlist.id}"
-  end
-
-  get '/show_wish_list/:id' do
-    if !Helper.is_logged_in?(session)
-      redirect '/login'
-    else
+    post '/new' do
       @user = Helper.current_user(session)
-      @newlist = WishList.find(params[:id])
-      erb :'show_wish_list'
+      @newlist = WishList.create(name: params[:name], user: @user)
+      redirect "/wish_list/show/#{@newlist.id}"
+    end
+
+    get '/show/:id' do
+      if !Helper.is_logged_in?(session)
+        redirect '/login'
+      else
+        @user = Helper.current_user(session)
+        @newlist = WishList.find(params[:id])
+        erb :'show_wish_list'
+      end
     end
   end
 end
