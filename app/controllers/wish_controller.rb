6390357916dbcee2a3b2c3f8043d4fd5174eaf86
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-12-06 17:53:55 -0500

    Namespaced wish controller to `/wish`.

--- app/controllers/wish_controller.rb
+++ app/controllers/wish_controller.rb
@@ -1,13 +1,15 @@
 class WishController <ApplicationController
   # use Rack::Flash
 
-  get '/add_wish_to_wishlist/:id' do
-    @wishlist = WishList.find(params[:id])
-    erb :'add_wish_to_wishlist'
-  end
+  namespace '/wish' do
+    get '/:wish_list_id/new' do
+      @wishlist = WishList.find(params[:wish_list_id])
+      erb :'add_wish_to_wishlist'
+    end
 
-  post '/add_wish_to_wishlist/:wish_list_id' do
-    @wish = Wish.create(name: params[:name], description: params[:description], url: params[:url], wish_list_id: params[:wish_list_id])
-    redirect "/show_wish_list/#{@wish.wish_list_id}"
+    post '/:wish_list_id/new' do
+      @wish = Wish.create(name: params[:name], description: params[:description], url: params[:url], wish_list_id: params[:wish_list_id])
+      redirect "/wish_list/show/#{@wish.wish_list_id}"
+    end
   end
 end
