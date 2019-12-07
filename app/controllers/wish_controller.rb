8931c3914ce7ca69b92fc011cc5b3a6e6b99698a
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-12-07 16:45:08 -0500

    Configures user autentication in `edit` and `new` routes.

--- app/controllers/wish_controller.rb
+++ app/controllers/wish_controller.rb
@@ -1,5 +1,5 @@
 class WishController <ApplicationController
-  # use Rack::Flash
+  use Rack::Flash
 
   namespace '/wish' do
     get '/:wish_list_id/new' do
@@ -8,8 +8,46 @@ class WishController <ApplicationController
     end
 
     post '/:wish_list_id/new' do
-      @wish = Wish.create(name: params[:name], description: params[:description], url: params[:url], wish_list_id: params[:wish_list_id])
-      redirect "/wish_list/show/#{@wish.wish_list_id}"
+      @user = Helper.current_user(session)
+      @wishlist = WishList.find(params[:wish_list_id])
+
+      if @user.id == @wishlist.user_id
+        @wish = Wish.create(
+          name: params[:name],
+          description: params[:description],
+          url: params[:url],
+          wish_list_id: params[:wish_list_id]
+        )
+
+        redirect "/wish_list/show/#{@wish.wish_list_id}"
+      else
+        flash[:message] = "oops, you don't seem to be the owner of this list. Only the owner of the list add wishes to it."
+        redirect "/list"
+      end
+    end
+
+    get '/:id/edit' do
+      @user = Helper.current_user(session)
+      @wish = Wish.find(params[:id])
+      erb :'edit_wish'
+    end
+
+    patch '/:id/edit' do
+
+      @user = Helper.current_user(session)
+      @wishlist = WishList.find(params[:wish_list_id])
+      @wish = Wish.find(params[:id])
+
+       if @user.id == @wishlist.user_id
+         @wish.name = params[:name]
+         @wish.description = params[:description]
+         @wish.url = params[:url]
+         @wish.save
+         redirect "/wish_list/show/#{@wish.wish_list_id}"
+       else
+         "oops, you don't seem to be the owner of this list. Only the owner of the list edit it."
+         redirect '/list'
+       end
     end
   end
 end
