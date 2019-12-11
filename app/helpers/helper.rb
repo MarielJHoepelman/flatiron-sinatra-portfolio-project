7c8126e38c86ab511f9fa16bc3a741576fcb6af1
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-12-11 16:45:54 -0500

    Configures email format verification method.

--- app/helpers/helper.rb
+++ app/helpers/helper.rb
@@ -6,4 +6,10 @@ class Helper
   def self.is_logged_in?(session_hash)
     !!session_hash[:user_id]
   end
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  def self.is_valid_email?(email)
+    !!(email =~ VALID_EMAIL_REGEX)
+  end
 end
