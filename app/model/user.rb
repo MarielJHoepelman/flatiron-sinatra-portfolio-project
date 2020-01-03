c1f82d32ebfb6f1eb62559f9b0f7efa6995f8fca
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-01-03 18:57:01 -0500

    Validates username format. Adds error messages.

--- app/model/user.rb
+++ app/model/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
-  validates :username, uniqueness: { case_sensitive: false }
-  validates :email, uniqueness: { case_sensitive: false }
+  validates :username, uniqueness: { case_sensitive: false,
+    message: "Username taken." }
+  validates :username, format: { with: /\A[a-zA-Z0-9]+([._]?[a-zA-Z0-9]+)*\z/,
+    message: "Invalid username. Please try again." }
+  validates :email, uniqueness: { case_sensitive: false,
+    message: "There is an account with this email."  }
 
   has_secure_password
   has_many :wishlists
