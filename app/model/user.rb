b47e83cb8db0b295c982020006641b67b3c41e20
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-01-03 17:49:02 -0500

    Validates username and email uniqueness.

--- app/model/user.rb
+++ app/model/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  validates :username, uniqueness: { case_sensitive: false }
+  validates :email, uniqueness: { case_sensitive: false }
+
   has_secure_password
-  has_many :wishlists 
+  has_many :wishlists
 end
