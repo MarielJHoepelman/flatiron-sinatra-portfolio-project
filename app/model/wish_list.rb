e45639b4cb20f44771961e9190e78d035f60a59a
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-01-07 12:55:52 -0500

    Validates presence of name in WishList model.

--- app/model/wish_list.rb
+++ app/model/wish_list.rb
@@ -2,6 +2,8 @@ class WishList < ActiveRecord::Base
   belongs_to :user
   has_many :wishes
 
+  validates :name, presence: { message: "Name field cannot be blank." }
+
   def is_own_list?(session_user_id)
     user_id == session_user_id
   end
