1e78d83da22e7e63be26ba199bdf1e5142949646
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-01-08 15:33:35 -0500

    Adds url format validation to Wish model.

--- app/model/wish.rb
+++ app/model/wish.rb
@@ -1,4 +1,6 @@
 class Wish < ActiveRecord::Base
   validates :name, :description, :url, presence: { message: "Fields cannot be blank." }
+  validates :url, format: { with: /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?\z/,
+    message: "Invalid url. Please try again." }
   belongs_to :wishlist
 end
