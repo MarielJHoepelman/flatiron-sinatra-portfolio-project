3bba4bec69eb19573c3fe823dbef875e87eec38b
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-01-08 17:35:52 -0500

    Removes username validations from User model. Adds name validation.

--- app/model/user.rb
+++ app/model/user.rb
@@ -1,16 +1,13 @@
 class User < ActiveRecord::Base
-  validates :username, presence: { message: "Username field cannot be blank." }
-  validates :username, uniqueness: { case_sensitive: false,
-    message: "Username taken." }
-  validates :username, format: { with: /\A[a-zA-Z0-9]+([._]?[a-zA-Z0-9]+)*\z/i,
-    message: "Invalid username. Please try again." }
+  validates :name, presence: { message: "Name field cannot be blank." }
 
   validates :email, presence: { message: "Email field cannot be blank." }
   validates :email, uniqueness: { case_sensitive: false,
     message: "There is an account with this email."  }
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
       message: "Invalid email format. Please try again." }
-  validates :password_digest, presence: { message: "Password field cannot be blank." }    
+
+  validates :password_digest, presence: { message: "Password field cannot be blank." }
 
   has_secure_password
   has_many :wishlists
