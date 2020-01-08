b9f497cfbb4d71629ab2359f51fd715211365f83
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-01-08 15:32:57 -0500

    Removes email validation method from helper class.

--- app/helpers/helper.rb
+++ app/helpers/helper.rb
@@ -6,10 +6,4 @@ class Helper
   def self.is_logged_in?(session_hash)
     !!session_hash[:user_id]
   end
-
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-
-  def self.is_valid_email?(email)
-    !!(email =~ VALID_EMAIL_REGEX)
-  end
 end
