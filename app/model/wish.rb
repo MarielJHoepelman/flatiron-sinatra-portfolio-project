4a8f5dc21a757241ec7dff122aaa14c577cf48cd
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-02-01 17:03:50 -0500

    Creates 'safe_url' in Wish model.

--- app/model/wish.rb
+++ app/model/wish.rb
@@ -3,4 +3,8 @@ class Wish < ActiveRecord::Base
   validates :url, format: { with: /\A(https:\/\/|http:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?\z/,
     message: "Invalid url. Please try again." }
   belongs_to :wishlist
+
+  def safe_url
+    url.match(/(http|https)/) ? url : "https://#{url}"
+  end
 end
